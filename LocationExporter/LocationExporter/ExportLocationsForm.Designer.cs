d3c8e4097ccc9b2b19af3f8eeeec6d0af24825f9
Steve Simon <ssimo@STEVESLAPTOP>
2024-01-17 13:45:58 -0500

    Added Install File (.msi) support.

--- LocationExporter/LocationExporter/ExportLocationsForm.Designer.cs
+++ LocationExporter/LocationExporter/ExportLocationsForm.Designer.cs
@@ -41,9 +41,10 @@
             // 
             // Export_Button
             // 
-            this.Export_Button.Location = new System.Drawing.Point(49, 137);
+            this.Export_Button.Location = new System.Drawing.Point(131, 327);
+            this.Export_Button.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.Export_Button.Name = "Export_Button";
-            this.Export_Button.Size = new System.Drawing.Size(136, 35);
+            this.Export_Button.Size = new System.Drawing.Size(363, 83);
             this.Export_Button.TabIndex = 0;
             this.Export_Button.Text = "Export";
             this.Export_Button.UseVisualStyleBackColor = true;
@@ -51,9 +52,10 @@
             // 
             // Quit_button
             // 
-            this.Quit_button.Location = new System.Drawing.Point(329, 137);
+            this.Quit_button.Location = new System.Drawing.Point(877, 327);
+            this.Quit_button.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.Quit_button.Name = "Quit_button";
-            this.Quit_button.Size = new System.Drawing.Size(128, 35);
+            this.Quit_button.Size = new System.Drawing.Size(341, 83);
             this.Quit_button.TabIndex = 1;
             this.Quit_button.Text = "Quit";
             this.Quit_button.UseVisualStyleBackColor = true;
@@ -61,9 +63,10 @@
             // 
             // FileChooser_Button
             // 
-            this.FileChooser_Button.Location = new System.Drawing.Point(28, 12);
+            this.FileChooser_Button.Location = new System.Drawing.Point(75, 29);
+            this.FileChooser_Button.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.FileChooser_Button.Name = "FileChooser_Button";
-            this.FileChooser_Button.Size = new System.Drawing.Size(229, 28);
+            this.FileChooser_Button.Size = new System.Drawing.Size(611, 67);
             this.FileChooser_Button.TabIndex = 2;
             this.FileChooser_Button.Text = "Chose Output Directory";
             this.FileChooser_Button.UseVisualStyleBackColor = true;
@@ -72,51 +75,57 @@
             // progressLabel
             // 
             this.progressLabel.AutoSize = true;
-            this.progressLabel.Location = new System.Drawing.Point(38, 79);
+            this.progressLabel.Location = new System.Drawing.Point(101, 188);
+            this.progressLabel.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
             this.progressLabel.Name = "progressLabel";
-            this.progressLabel.Size = new System.Drawing.Size(48, 13);
+            this.progressLabel.Size = new System.Drawing.Size(127, 32);
             this.progressLabel.TabIndex = 4;
             this.progressLabel.Text = "Progress";
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(39, 106);
+            this.progressBar1.Location = new System.Drawing.Point(104, 253);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(396, 17);
+            this.progressBar1.Size = new System.Drawing.Size(1056, 41);
             this.progressBar1.TabIndex = 5;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(282, 18);
+            this.label1.Location = new System.Drawing.Point(752, 43);
+            this.label1.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.Size = new System.Drawing.Size(105, 32);
             this.label1.TabIndex = 6;
             this.label1.Text = "Region";
             // 
             // comboBox_Regions
             // 
             this.comboBox_Regions.FormattingEnabled = true;
-            this.comboBox_Regions.Location = new System.Drawing.Point(329, 15);
+            this.comboBox_Regions.Location = new System.Drawing.Point(877, 36);
+            this.comboBox_Regions.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.comboBox_Regions.Name = "comboBox_Regions";
-            this.comboBox_Regions.Size = new System.Drawing.Size(121, 21);
+            this.comboBox_Regions.Size = new System.Drawing.Size(316, 39);
             this.comboBox_Regions.TabIndex = 7;
             this.comboBox_Regions.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // label_OutputDirectory
             // 
             this.label_OutputDirectory.AutoSize = true;
-            this.label_OutputDirectory.Location = new System.Drawing.Point(36, 49);
+            this.label_OutputDirectory.Location = new System.Drawing.Point(96, 117);
+            this.label_OutputDirectory.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
             this.label_OutputDirectory.Name = "label_OutputDirectory";
-            this.label_OutputDirectory.Size = new System.Drawing.Size(87, 13);
+            this.label_OutputDirectory.Size = new System.Drawing.Size(228, 32);
             this.label_OutputDirectory.TabIndex = 8;
             this.label_OutputDirectory.Text = "Output Directory:";
             // 
             // Setup_button
             // 
-            this.Setup_button.Location = new System.Drawing.Point(206, 137);
+            this.Setup_button.Location = new System.Drawing.Point(549, 327);
+            this.Setup_button.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.Setup_button.Name = "Setup_button";
-            this.Setup_button.Size = new System.Drawing.Size(103, 35);
+            this.Setup_button.Size = new System.Drawing.Size(275, 83);
             this.Setup_button.TabIndex = 9;
             this.Setup_button.Text = "Setup";
             this.Setup_button.UseVisualStyleBackColor = true;
@@ -124,9 +133,9 @@
             // 
             // LocationExportDialog
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(485, 185);
+            this.ClientSize = new System.Drawing.Size(1293, 441);
             this.Controls.Add(this.Setup_button);
             this.Controls.Add(this.label_OutputDirectory);
             this.Controls.Add(this.comboBox_Regions);
@@ -136,6 +145,8 @@
             this.Controls.Add(this.FileChooser_Button);
             this.Controls.Add(this.Quit_button);
             this.Controls.Add(this.Export_Button);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.Name = "LocationExportDialog";
             this.Text = "Breakthru Beverage Location Export Tool";
             this.ResumeLayout(false);
